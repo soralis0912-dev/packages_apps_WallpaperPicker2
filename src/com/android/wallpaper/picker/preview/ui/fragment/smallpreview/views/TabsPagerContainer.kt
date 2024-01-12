76b8c67648713674f3e5fb195dfecb71e21a5a8a
abdullahirum <abdullahirum@google.com>
2024-01-12 21:58:43 +0000

    Ignore touch events outside of tabs
    This change makes the tab container intercept touch events (out side of the tabs' view rec) that belong to a gesture (sequence of touches) and effectively ignore the entire gesture
    
    Flag: ACONFIG com.android.wallpaper.multi_crop_preview_ui_flag DEVELOPMENT
    Bug: 319911023
    Test: manually verified
    Change-Id: I8c53ca9f661a70f484bf6955d0eda69db55d65ec

--- src/com/android/wallpaper/picker/preview/ui/fragment/smallpreview/views/TabsPagerContainer.kt
+++ src/com/android/wallpaper/picker/preview/ui/fragment/smallpreview/views/TabsPagerContainer.kt
@@ -16,10 +16,13 @@
 package com.android.wallpaper.picker.preview.ui.fragment.smallpreview.views
 
 import android.content.Context
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.MotionEvent
+import android.view.View
 import android.widget.FrameLayout
+import androidx.core.view.get
 import androidx.viewpager.widget.ViewPager
 
 /** This container view wraps {TabsPager}. This is used to control the touch area of {TabsPager}. */
@@ -47,17 +50,45 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
         addView(viewPager)
     }
 
-    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+    // x-coordinate of the beginning of a gesture
+    var touchDownX = 0
+
+    // y-coordinate of the beginning of a gesture
+    var touchDownY = 0
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        val tab1Rect = viewPager.get(0).getViewRect()
+        val tab2Rect = viewPager.get(1).getViewRect()
+        when (ev.action) {
+            MotionEvent.ACTION_DOWN -> {
+                touchDownX = ev.getRawX().toInt()
+                touchDownY = ev.getRawY().toInt()
+
+                // if the touch down event doesn't land in the visible rect of either tab buttons
+                // then we ignore the entirety of the next gesture
+                if (
+                    !tab2Rect.contains(touchDownX, touchDownY) &&
+                        !tab1Rect.contains(touchDownX, touchDownY)
+                ) {
+                    return false
+                }
+            }
+        }
         return viewPager.dispatchTouchEvent(ev)
     }
 
-    // TODO: handle click button here and pass click to correct tab
-    override fun performClick(): Boolean {
-        super.performClick()
-        return true
+    private fun View.getViewRect(): Rect {
+        val returnRect = Rect()
+        this.getGlobalVisibleRect(returnRect)
+        return returnRect
     }
 
     fun getViewPager(): ViewPager {
         return viewPager
+    }
+
+    override fun performClick(): Boolean {
+        // returning true to signify that the click was handled by a custom implementation
+        return true
     }
 }
