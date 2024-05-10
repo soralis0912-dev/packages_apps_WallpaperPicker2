8cb0b6026732f460e3031cd207ef6335af906295
George Lin <giolin@google.com>
2024-05-10 18:42:51 +0000

    [New picker UI] Motion layout coordinating preview pager
    1. Introduce the preview pager
    2. Make the preview pager work with the scroll view
    
    Test: Manually teseted. See bug.
    Bug: 337941334
    Flag: com.android.wallpaper.new_picker_ui_flag
    Change-Id: I2cf87cd22fd5a6b2f79ce86de6697a6382eb1b5f

--- src/com/android/wallpaper/picker/customization/ui/CustomizationPickerActivity2.kt
+++ src/com/android/wallpaper/picker/customization/ui/CustomizationPickerActivity2.kt
@@ -16,10 +16,18 @@
 
 package com.android.wallpaper.picker.customization.ui
 
+import android.graphics.Color
+import android.graphics.Point
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.doOnLayout
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.android.wallpaper.R
 import com.android.wallpaper.module.MultiPanesChecker
+import com.android.wallpaper.picker.customization.ui.view.adapter.PreviewPagerAdapter
+import com.android.wallpaper.picker.customization.ui.view.transformer.PreviewPagerPageTransformer
 import com.android.wallpaper.util.ActivityUtils
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -50,5 +58,36 @@ class CustomizationPickerActivity2 : Hilt_CustomizationPickerActivity2() {
         }
 
         setContentView(R.layout.activity_cusomization_picker2)
+        initPreviewPager()
+    }
+
+    private fun initPreviewPager() {
+        val pager = requireViewById<ViewPager2>(R.id.preview_pager)
+        pager.apply {
+            adapter = PreviewPagerAdapter { viewHolder, position ->
+                viewHolder.itemView
+                    .requireViewById<View>(R.id.preview_card)
+                    .setBackgroundColor(if (position == 0) Color.BLUE else Color.CYAN)
+            }
+            // Disable over scroll
+            (getChildAt(0) as RecyclerView).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+            // The neighboring view should be inflated when pager is rendered
+            offscreenPageLimit = 1
+            // When pager's height changes, request transform to recalculate the preview offset
+            // to make sure correct space between the previews.
+            addOnLayoutChangeListener { view, _, _, _, _, _, topWas, _, bottomWas ->
+                val isHeightChanged = (bottomWas - topWas) != view.height
+                if (isHeightChanged) {
+                    pager.requestTransform()
+                }
+            }
+        }
+
+        // Only when pager is laid out, we can get the width and set the preview's offset correctly
+        pager.doOnLayout {
+            (it as ViewPager2).apply {
+                setPageTransformer(PreviewPagerPageTransformer(Point(width, height)))
+            }
+        }
     }
 }
